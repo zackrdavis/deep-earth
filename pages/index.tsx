c40adf1f514688b0d9d755dbc5217251214376c6
Zack Davis <zackrdavis@gmail.com>
2022-04-01 19:45:51 -0700

    detect touch on landing image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,64 @@
 import type { NextPage, GetStaticProps } from "next";
+import { useState, useEffect } from "react";
+import styled, { keyframes } from "styled-components";
 import { Footer } from "../components/Footer";
 
-interface Props {
-  content: { attributes: HomeAttributes };
-}
-interface HomeAttributes {
-  landing_image: string;
+type HomeSection = {
+  text: string;
+  image: string;
+};
+
+interface HomeProps {
+  content: {
+    attributes: {
+      landing_image: string;
+      sections: HomeSection[];
+    };
+  };
 }
 
-const Home: NextPage<Props> = ({ content }) => {
+const StyledLandingImage = styled.img`
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  top: 0%;
+  opacity: 1;
+  transition: all 1s;
+  cursor: pointer;
+
+  &.touched {
+    opacity: 0;
+  }
+`;
+
+const Home: NextPage<HomeProps> = ({ content }) => {
   const { attributes } = content;
+
+  const [touched, setTouched] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("click", () => setTouched(true));
+    window.addEventListener("mousewheel", () => setTouched(true));
+    return () => {
+      window.removeEventListener("click", () => setTouched(true));
+      window.removeEventListener("mousewheel", () => setTouched(true));
+    };
+  }, []);
+
   return (
     <>
-      <img src={attributes.landing_image} />
+      <StyledLandingImage
+        className={touched ? "touched" : ""}
+        src={attributes.landing_image}
+      />
       <Footer />
+      {attributes.sections.map((section) => (
+        <div>
+          {section.text}
+          <img src={section.image} />
+        </div>
+      ))}
     </>
   );
 };
