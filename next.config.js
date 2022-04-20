a90488757c0446f8a46a3332b7e2761b7ea738a9
Zack Davis <zackrdavis@gmail.com>
2022-04-20 12:47:41 -0700

    add styledComponents:true to next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  styledComponents: true,
   webpack: (config) => {
     config.module.rules.push({
       test: /\.md$/,
