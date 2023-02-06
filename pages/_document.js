298d70fad9bd9d3d9132cb4d6bbcd7eb3e585700
Zack Davis <zackrdavis@gmail.com>
2023-02-05 17:30:30 -0800

    favicon

--- pages/_document.js
+++ pages/_document.js
@@ -32,7 +32,26 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head></Head>
+        <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/site_images/pin.svgapple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/site_images/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/site_images/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site_images/site.webmanifest" />
+        </Head>
         <body>
           <Main />
           <NextScript />
